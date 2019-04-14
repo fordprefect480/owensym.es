b1f85a5bb8e04f6c99f45177ffd82ac4fb3d6e7a
Owen Symes <owen.symes@gmail.com>
2019-04-14 21:05:56 +0930

    Fleshed out projects page.

--- owensym.es/Controllers/HomeController.cs
+++ owensym.es/Controllers/HomeController.cs
@@ -14,7 +14,11 @@ namespace owensym.es.Controllers
         {
             return View();
         }
-        
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
